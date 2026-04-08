cdf79f37c4947d741b4ed1805b218d61240fbce6
Nandani2024 <162008931+Nandani2024@users.noreply.github.com>
2026-04-08 13:03:51 +0530

    Update Main.java

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -4,7 +4,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+         for (int i = 0; i < n; i++) {
+    int x = sc.nextInt();
 
+    // Check Prime
+    boolean isPrime = true;
+    if (x <= 1) isPrime = false;
+    for (int j = 2; j * j <= x; j++) {
+        if (x % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    // Check Perfect
+    int sum = 0;
+    for (int j = 1; j <= x / 2; j++) {
+        if (x % j == 0) {
+            sum += j;
+        }
+    }
+    boolean isPerfect = (sum == x && x != 0);
+
+    // Output
+    if (isPrime && isPerfect) {
+        System.out.println("Both");
+    } else if (isPrime) {
+        System.out.println("Prime");
+    } else if (isPerfect) {
+        System.out.println("Perfect");
+    } else {
+        System.out.println("Neither");
+    }
+}
         // TODO: Read n integers. For each number print:
         //   "Prime"     if it is prime
         //   "Perfect"   if it is a perfect number (sum of proper divisors == itself, e.g. 6=1+2+3)
