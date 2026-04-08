f93b38684946611a3c1b6e8cf699a5eceaaf61b2
Nandani2024 <162008931+Nandani2024@users.noreply.github.com>
2026-04-08 13:05:23 +0530

    Update Main.java

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int a = 0, b = 1;
 
+for (int i = 0; i < n; i++) {
+    System.out.print(a);
+    
+    if (i < n - 1) {
+        System.out.print(" ");
+    }
+    
+    int next = a + b;
+    a = b;
+    b = next;
+}
         // TODO: Print first n Fibonacci numbers separated by spaces
         //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
         //
