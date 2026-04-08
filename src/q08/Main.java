e8b2658d73d06d3cc448b936a7b5814e017f095c
Nandani2024 <162008931+Nandani2024@users.noreply.github.com>
2026-04-08 13:06:53 +0530

    Update Main.java

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -4,7 +4,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+int[] stack = new int[n];
+int top = -1;
 
+for (int i = 0; i < n; i++) {
+    String op = sc.next();
+
+    if (op.equals("PUSH")) {
+        int x = sc.nextInt();
+        top++;
+        stack[top] = x;
+
+    } else if (op.equals("POP")) {
+        if (top == -1) {
+            System.out.println("EMPTY");
+        } else {
+            System.out.println(stack[top]);
+            top--;
+        }
+
+    } else if (op.equals("PEEK")) {
+        if (top == -1) {
+            System.out.println("EMPTY");
+        } else {
+            System.out.println(stack[top]);
+        }
+
+    } else if (op.equals("SIZE")) {
+        System.out.println(top + 1);
+    }
+}
         // TODO: Implement a stack using an array (no Java Stack class)
         //       Process n operations:
         //         PUSH x  → push integer x
