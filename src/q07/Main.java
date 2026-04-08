d1a6d2ae6f71bc755e157051f6cef7314708fb04
Nandani2024 <162008931+Nandani2024@users.noreply.github.com>
2026-04-08 13:06:07 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -4,7 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+          int[] arr = new int[n];
 
+// Input
+for (int i = 0; i < n; i++) {
+    arr[i] = sc.nextInt();
+}
+
+int swaps = 0;
+
+// Bubble Sort
+for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            // swap
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+            swaps++;
+        }
+    }
+}
+
+// Print sorted array
+for (int i = 0; i < n; i++) {
+    System.out.print(arr[i]);
+    if (i < n - 1) System.out.print(" ");
+}
+System.out.println();
+
+// Print swap count
+System.out.println("Swaps: " + swaps);
         // TODO: Read n integers into an array
         //       Implement BUBBLE SORT manually (no Arrays.sort)
         //       Print sorted array ascending, space-separated
